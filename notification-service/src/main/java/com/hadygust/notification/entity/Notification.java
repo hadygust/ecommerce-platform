ad7a4cf6abfd901282c0047fcc8d1c133531fb20
hadygust <hadygust2883@gmail.com>
2026-05-16 16:11:36 +0700

    feat: notification entity

---
+++ notification-service/src/main/java/com/hadygust/notification/entity/Notification.java
@@ -0,0 +1,36 @@
+package com.hadygust.notification.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID eventId;
+
+    private UUID userId;
+
+    private String userEmail;
+
+    private String eventType;
+
+    @Column(columnDefinition = "TEXT")
+    private String payload;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+}
